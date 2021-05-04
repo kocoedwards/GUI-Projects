b393702a4739c898bd97175a8bab6e6608338177
kocoedwards <79869557+kocoedwards@users.noreply.github.com>
2021-05-04 11:47:20 -0600

    Add files via upload
    Mine tells me there's an error, but I don't know why. I did follow along very well though, and I understood most of the lesson today.

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,13 +1,28 @@
 from tkinter import *
 
 top = Tk()
+groceryList = []
 
-L1 = Label(top, text="Hello, world!")
+def results():
+    print(groceryList)
 
+def addToList():
+    newItem = E1.get()
+    groceryList.append(newItem)
+
+#This is a Label widget
+L1 = Label(top, text = "Grocery List")
 L1.grid(column = 0, row = 1)
 
+#This is a Entry widget
 E1 = Entry(top, bd = 5)
-
 E1.grid(column = 0, row = 2)
+
+#This is a Button widget
+B1 = Button(text = "    Print List    ", bg = "green", command = results)
+B1.grid(column = 0, row = 3)
+
+B2 = Button(text = "    Add to List    ", bg = "green", comand = addToList)
+B2.grid(column = 1, row = 2)
 
 top.mainloop()
